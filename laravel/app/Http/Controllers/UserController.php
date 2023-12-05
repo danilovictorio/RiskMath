2bf101d47ab7756a317523e549e4d86cf9935082
danilovictorio <a17danvicfer@inspedralbes.cat>
2023-12-05 12:05:15 +0100

    adduser-deleteuser

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -3,38 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User; 
+use App\Models\User;
+
 class UserController extends Controller
 {
     public function addUser(Request $request)
     {
-     
+
         $request->validate([
             'nombreUsuario' => 'required|string',
         ]);
-    
+
         $nuevoUsuario = new User();
         $nuevoUsuario->name = $request->input('nombreUsuario');
-      
+
         $nuevoUsuario->save();
-    
+
         return response()->json(['mensaje' => 'Usuario agregado correctamente']);
-    } 
-    public function removeUser($name){
-
-    $usuario = User::find($name);
-
-    if ($usuario) {
-        $usuario->delete();
-
-        return response()->json(['mensaje' => 'Usuario eliminado correctamente']);
-    } else {
-        return response()->json(['mensaje' => 'Usuario no encontrado'], 404);
     }
+
+    
+    public function removeUser($nom)
+    {
+      
+        $usuario = User::where('name', $nom)->first();
+    
+        if ($usuario) {
+         
+            $usuario->delete();
+    
+            return response()->json(['mensaje' => 'Usuario eliminado correctamente']);
+        } else {
+            return response()->json(['mensaje' => 'Usuario no encontrado'], 404);
+        }
     }
+    public function updateUser($user){
+
+    }    
 }
 
-    
+
 
 
 
