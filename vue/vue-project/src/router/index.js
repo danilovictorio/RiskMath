9b8cbefc517122311c3ee18fd4177b5311a47aad
MarioBenPedralbes <a21marbenalc@inspedralbes.cat>
2023-12-15 09:53:48 +0100

    router fixing

--- vue/vue-project/src/router/index.js
+++ vue/vue-project/src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../components/PaginaPrincipal.vue'
 import TaulerView from '../components/TaulerView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
