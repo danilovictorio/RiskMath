e6ea763215c628300be5b512082b77a000e592b7
galad22 <a19galdelred@inspedralbes.cat>
2024-04-25 11:54:48 +0200

    gfht

--- nuxt/.nuxt/dist/server/client.manifest.mjs
+++ nuxt/.nuxt/dist/server/client.manifest.mjs
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export { default } from "file:///C:/Users/Argo/Desktop/trfinal/tr2-MultipliCAT-G7/nuxt/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
-=======
 export { default } from "file:///C:/Users/Argo/Desktop/tr-final-este/tr2-MultipliCAT-G7/nuxt/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
->>>>>>> 0b85cc560cfc4c2243eea4ad32e06f7527d8acf1
