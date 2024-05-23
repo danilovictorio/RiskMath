bb97efeea3d3659430206fa7167083f939fb97f3
danilovictorio <a17danvicfer@inspedralbes.cat>
2024-05-23 18:46:33 +0200

    eliminacion de taincolor

--- nuxt/pages/TaulerView.vue
+++ nuxt/pages/TaulerView.vue
@@ -162,7 +162,7 @@
 import { obtenerPaises, enviarAtac, confirmarAtaque, validarResposta } from '../services/communicationManager.js';
 import { socket } from '@/utils/socket.js';
 import { useAppStore } from '../stores/app';
-import tinycolor from 'tinycolor2';
+//import tinycolor from 'tinycolor2';
 export default {
   data() {
     return {
@@ -225,17 +225,18 @@ export default {
         fillColor = paisElement.getAttribute('fill');
       }
 
-      const turnoColorHex = tinycolor(this.app.turnoDe.color).toHex(); // Convertir a hexadecimal
-      const turnoColorRGB = tinycolor(this.app.turnoDe.color).toRgbString();
+      //const turnoColorHex = tinycolor(this.app.turnoDe.color).toHex(); // Convertir a hexadecimal
+      //const turnoColorRGB = tinycolor(this.app.turnoDe.color).toRgbString();
       console.log("fillColor", fillColor);
       console.log("colorName", colorName);
-      console.log("turnoColorHex", turnoColorHex);
-      console.log("turnoColorRGB", turnoColorRGB);
+      //console.log("turnoColorHex", turnoColorHex);
+      //console.log("turnoColorRGB", turnoColorRGB);
       console.log(this.app.turnoDe.color);
 
       if (this.app.esMiturno()) {
-        if (colorName === this.app.turnoDe.color || fillColor === turnoColorHex || fillColor === turnoColorRGB) {
+        if (colorName === this.app.turnoDe.color) {
           console.log("El país ya está conquistado por ti.");
+          this.clicksDeshabilitados = false;
         } else if (fillColor === '#ffffff' || fillColor === 'rgb(255, 255, 255)' || colorName == 'white') { // Color blanco
           console.log("El país no está conquistado.");
           this.enviarAtac(idPais, name, idUser);
